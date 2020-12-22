e0fecec996289667f14f9940ce870ee7dd87f563
Moon <noelmyname0714@gmail.com>
2020-12-22 19:37:39 +0900

    .
    .

---
+++ SWEA/4839/이진탐색.py
@@ -0,0 +1,67 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+a = int(input())                        정수형 변수 1개 입력 받는 예제
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+
+# 표준 출력 예제
+'''
+a, b = 6, 3
+c, d, e = 1.0, 2.5, 3.4
+f = "ABC"
+print(a)                                정수형 변수 1개 출력하는 예제
+print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
+print(c, d, e)                          실수형 변수 3개 출력하는 예제
+print(f)                                문자열 1개 출력하는 예제
+'''
+
+import sys
+
+
+def A(l, r, Pa):
+    count = 0
+
+    while True:
+        c = int((l+r)/2)
+
+        if c == l or c == r:
+            break
+
+        if c == Pa:
+            return count + 1
+        elif c < Pa:
+            l = c
+        else:
+            r = c
+
+        count += 1
+
+    return 99999
+
+
+sys.stdin = open("sample_input.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    r, Pa, Pb = map(int, input().split())
+
+    a = A(1, r, Pa)
+    b = A(1, r, Pb)
+
+    if a < b:
+        print(f"#{test_case} A")
+    elif a == b:
+        print(f"#{test_case} 0")
+    elif a > b:
+        print(f"#{test_case} B")
+
+
+    # ///////////////////////////////////////////////////////////////////////////////////
