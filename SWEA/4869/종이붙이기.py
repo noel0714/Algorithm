acdb2ea4628449415b9bc431865ccc6710728ffb
Moon <noelmyname0714@gmail.com>
2021-01-06 15:18:27 +0900

    .
    .

---
+++ SWEA/4869/종이붙이기.py
@@ -0,0 +1,50 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+a = int(input())                        정수형 변수 1개 입력 받는 예제
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+
+# 표준 출력 예제
+'''
+a, b = 6, 3
+c, d, e = 1.0, 2.5, 3.4
+f = "ABC"
+print(a)                                정수형 변수 1개 출력하는 예제
+print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
+print(c, d, e)                          실수형 변수 3개 출력하는 예제
+print(f)                                문자열 1개 출력하는 예제
+'''
+
+import sys
+
+
+def block():
+    block_num = [1]
+    sum = 1
+
+    for i in range(15):
+        block_num.append(sum + 2)
+        sum = sum + block_num[-1]
+
+        block_num.append(sum + 1)
+        sum = sum + block_num[-1]
+
+
+    return block_num
+
+sys.stdin = open("sample_input.txt", "r")
+
+block_num = block()
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(int(input()) / 10)
+
+    print(f"#{test_case} {block_num[N-1]}")
